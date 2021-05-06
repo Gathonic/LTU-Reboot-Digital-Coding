520abbe8e515c6c12126f6ac45ed9b027a73b37c
Gathonic <cnelgm@yahoo.com>
2021-05-06 11:55:15 +0100

    Update main.js
    Created switch statement
    Added drink order
    Added Calculator

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -46,4 +46,86 @@ function productDiscount() {
     } 
 }
 
-productDiscount();
+//productDiscount();
+
+/*
+check drink
+ coke
+ lemonade
+ orangeade
+ default- what if none of those drinks are selected
+ what if the drink is in capitals?
+ lowercase() = lowercase
+*/
+
+
+
+function drinkOrder (drink, size) {
+    let drinkLow = drink.toLowerCase();
+
+    switch (drinkLow) {
+        case 'coke':
+            console.log(drink, size); // coke
+            break;
+
+        case 'lemonade' :
+            console.log(drinkLow, size); // lemonade
+            break;
+
+        case 'orangeade' :
+            console.log(drinkLow, size); // orangeade
+            break;
+
+        default:
+            console.log(drinkLow, 'is not available');
+            break;
+    }
+}
+
+//drinkOrder('Lemonade', 'extra large');
+
+/*
+operator
++ - / *
+*/
+
+
+
+function calculator(num1, num2, operator){
+
+    switch (operator) {
+        case '+':
+            //console.log(num1 + num2); // addition
+            //console.log(num1 + '+' + num2 + '=' + (num1+num2) );
+            console.log(`${num1} ${operator} ${num2} = ${num1 + num2}` );
+            break;
+
+        case '-':
+            console.log(`${num1} ${operator} ${num2} = ${num1 - num2}`); // subtraction
+                break;
+        
+        case '/':
+            console.log(`${num1} ${operator} ${num2} = ${num1 / num2}`); //division
+                break;
+        
+        case '*':
+            console.log(`${num1} ${operator} ${num2} = ${num1 * num2}`);  // multiplication
+                break;
+        
+        case '%':
+            console.log(`${num1} ${operator} ${num2} = ${num1 % num2}`);  // modulus
+                break;
+    
+        default:
+            console.log(`${operator} doesnt exist`);
+            break;
+    } // end of switch statement 
+
+} // end of calculator function
+
+calculator(5, 5, '+');
+calculator(5, 5, '-');
+calculator(5, 5, '/');
+calculator(5, 5, '*');
+calculator(5, 3, '%');
+calculator(5, 3, '?');
